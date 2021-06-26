71e0c1d342044a50c037ed116772716a79563d0a
Gregorio Rojas <gregorio@sabio.la>
2021-06-26 09:36:28 -0700

    bs 5 and updated packages

--- src/App.jsx
+++ src/App.jsx
@@ -6,90 +6,102 @@ class App extends Component {
   render() {
     return (
       <React.Fragment>
-        <nav className="navbar navbar-expand-md navbar-dark fixed-top bg-dark sabio">
-          <button className="link-button navbar-brand">Navbar</button>
-          <button
-            className="navbar-toggler"
-            type="button"
-            data-toggle="collapse"
-            data-target="#navbarsExampleDefault"
-            aria-controls="navbarsExampleDefault"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
-          >
-            <span className="navbar-toggler-icon"></span>
-          </button>
-
-          <div className="collapse navbar-collapse" id="navbarsExampleDefault">
-            <ul className="navbar-nav mr-auto">
-              <li className="nav-item active">
-                <button className="nav-link link-button">
-                  Home <span className="sr-only">(current)</span>
-                </button>
-              </li>
-              <li className="nav-item">
-                <button className="nav-link link-button">Link</button>
-              </li>
-              <li className="nav-item">
-                <button className="nav-link disabled">Disabled</button>
-              </li>
-              <li className="nav-item dropdown">
-                <a
-                  className="nav-link dropdown-toggle"
-                  href="http://example.com"
-                  id="dropdown01"
-                  data-toggle="dropdown"
-                  aria-haspopup="true"
-                  aria-expanded="false"
-                >
-                  Dropdown
-                </a>
-                <div className="dropdown-menu" aria-labelledby="dropdown01">
-                  <button className="dropdown-item  link-button">Action</button>
-                  <button className="dropdown-item link-button">
-                    Another action
-                  </button>
-                  <button className="dropdown-item  link-button">
-                    Something else here
-                  </button>
-                </div>
-              </li>
-            </ul>
-            <form className="form-inline my-2 my-lg-0">
-              <input
-                className="form-control mr-sm-2"
-                type="text"
-                placeholder="Search"
-                aria-label="Search"
-              />
-              <button
-                className="btn btn-outline-success my-2 my-sm-0"
-                type="submit"
+        <header className="p-3 bg-dark text-white">
+          <div className="container">
+            <div className="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
+              <a
+                href="/"
+                className="d-flex align-items-center mb-2 mb-lg-0 text-white text-decoration-none"
               >
-                Search
-              </button>
-            </form>
-          </div>
-        </nav>
+                <img
+                  src="https://pw.sabio.la/images/Sabio.png"
+                  width="30"
+                  height="30"
+                  className="d-inline-block align-top"
+                  alt="Sabio"
+                />
+              </a>
 
-        <main role="main">
-          <div className="jumbotron">
-            <div className="container">
-              <h1 className="display-3">Hello, world!</h1>
-              <p>
-                This is a template for a simple marketing or informational
-                website. It includes a large callout called a jumbotron and
-                three supporting pieces of content. Use it as a starting point
-                to create something more unique.
-              </p>
-              <p>
-                <button className="btn btn-primary btn-lg">
-                  Learn more &raquo;
+              <ul className="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
+                <li>
+                  <button
+                    href="#"
+                    className="nav-link px-2 text-secondary link-button"
+                  >
+                    Home
+                  </button>
+                </li>
+                <li>
+                  <button className="nav-link px-2 text-white link-button">
+                    Features
+                  </button>
+                </li>
+                <li>
+                  <button
+                    href="#"
+                    className="nav-link px-2 text-white link-button"
+                  >
+                    Pricing
+                  </button>
+                </li>
+                <li>
+                  <button
+                    href="#"
+                    className="nav-link px-2 text-white link-button"
+                  >
+                    FAQs
+                  </button>
+                </li>
+                <li>
+                  <button
+                    href="#"
+                    className="nav-link px-2 text-white link-button"
+                  >
+                    About
+                  </button>
+                </li>
+              </ul>
+
+              <form className="col-12 col-lg-auto mb-3 mb-lg-0 me-lg-3">
+                <input
+                  type="search"
+                  className="form-control form-control-dark"
+                  placeholder="Search..."
+                  aria-label="Search"
+                />
+              </form>
+
+              <div className="text-end">
+                <button type="button" className="btn btn-outline-light me-2">
+                  Login
                 </button>
-              </p>
+                <button type="button" className="btn btn-warning">
+                  Sign-up
+                </button>
+              </div>
             </div>
           </div>
+        </header>
 
+        <main role="main">
+          <div className="container">
+            <div className="p-5 mb-4 bg-light rounded-3">
+              <div className="container-fluid py-5">
+                <h1 className="display-5 fw-bold">Hello, world!</h1>
+                <p className="col-md-8 fs-4">
+                  This is a template for a simple marketing or informational
+                  website. It includes a large callout called a jumbotron and
+                  three supporting pieces of content. Use it as a starting point
+                  to create something more unique.
+                </p>
+                <p>
+                  <button className="btn btn-primary btn-lg">
+                    Learn more &raquo;
+                  </button>
+                </p>
+              </div>
+            </div>
+          </div>
           <div className="container">
             <div className="row">
               <div className="col-md-4">
